b25f91671d3af0955aba0b20d6fdd66958f54473
jnmartin84 <jnmartin84@gmail.com>
2026-01-07 22:16:39 -0500

    i had thread priority backwards

--- src/pc/pc_main.c
+++ src/pc/pc_main.c
@@ -168,7 +168,7 @@ void main_func(void) {
     audio_attr.create_detached = 1;
 	audio_attr.stack_size = 32768;
 	audio_attr.stack_ptr = NULL;
-	audio_attr.prio = PRIO_DEFAULT + 1;
+	audio_attr.prio = PRIO_DEFAULT - 1;
 	audio_attr.label = "AudioSynthesis";
     thd_create_ex(&audio_attr, &AudioSynthesisThread, NULL);
 
